51ed6dbfcba9cc2a970d5e2e4cc0dab450735566
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-07-03 18:13:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID =27383453
+API_HASH ="4c246fb0c649477cc2e79b6a178ddfaa"
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-OWNER_ID = int(getenv("OWNER_ID"))
+BOT_TOKEN ="7376644777:AAHi8jMJUVk4AXC1otYG40_XM8mKiwicOYY"
+OWNER_ID =6352107773
 
-MONGO_DB_URI = getenv("MONGO_DB_URI")
-MUST_JOIN = getenv("MUST_JOIN", None)
+MONGO_DB_URI =mongodb+srv://BADMUNDA:BADMYDAD@badhacker.i5nw9na.mongodb.net/
+MUST_JOIN ="PUNJABI_CHATTING_HUB"
